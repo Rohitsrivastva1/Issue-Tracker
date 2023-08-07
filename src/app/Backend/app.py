d78e22ffac38d3808af96ac9a456c5a546e853bf
rohitsrivastva1 <rohit.managers@gmail.com>
2023-08-07 07:32:30 +0530

    extracted data from excel

--- src/app/Backend/app.py
+++ src/app/Backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import openpyxl
+import pandas as pd
 
 app = Flask(__name__)
 CORS(app)
@@ -63,6 +64,25 @@ def get_user_data():
     # else:
     #     return jsonify({'message': 'User not found'}), 404
 
+
+def load_issues():
+    excel_file = 'path/to/your/excel/file.xlsx'
+    df = pd.read_excel(excel_file)
+    user_data = {
+        'userName': 'rohit',
+        'circle': 'punjab',
+        'ba': 'ci',
+        'phone': '6394322640'
+        # Add more fields as needed
+    }
+    # return df.to_dict(orient='records')
+    return user_data
+
+
+@app.route('/api/issues', methods=['GET'])
+def get_issues():
+    issues = load_issues()
+    return jsonify(issues)
 
 if __name__ == '__main__':
     app.run()
